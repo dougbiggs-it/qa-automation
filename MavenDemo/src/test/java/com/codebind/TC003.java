e141c6888b8bb1b4de810e3d24292d4e63c1299f
doug <dougbiggs.it@gmail.com>
2020-01-13 14:36:08 -0500

    Commit TC003

---
+++ MavenDemo/src/test/java/com/codebind/TC003.java
@@ -0,0 +1,14 @@
+package com.codebind;
+
+import org.testng.annotations.Test;
+
+public class TC003 {
+
+	@Test
+	public void PrintOrder()
+	{
+		System.out.println("PrintOrder... TC003");
+	}
+	
+	
+}
